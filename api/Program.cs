4f4803700c5800e523de56c5625738ec4e90ad15
lchahal <123313031+lchahalx@users.noreply.github.com>
2026-01-15 22:54:11 -0500

    Update Program.cs

--- api/Program.cs
+++ api/Program.cs
@@ -23,6 +23,10 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
+var test_summaries = new[]
+{
+    "Freezing", "Bracing", "Balmy", "Hot", "Sweltering", "Scorching"
+};
 app.MapGet("/sensitive", (string? password) =>
 {
     return "Access denied!";
